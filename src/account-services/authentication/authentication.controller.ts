fa9edbe227c9e4d5dadcb0b089218a50b496c250
TuanAnh1003 <anhaanh2003@gmail.com>
2024-02-28 13:42:16 +0700

    feat: implement jwt auth guard

--- src/account-services/authentication/authentication.controller.ts
+++ src/account-services/authentication/authentication.controller.ts
@@ -6,35 +6,20 @@ import { LocalGuard } from './guards/local.guard';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { Request } from 'express';
 import { PermissionGuard } from './guards/permissions.guard';
-import { UsersService } from 'src/users/users.service';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @ApiTags('Authentication')
 @Controller('auth')
 export class AuthenticationController {
   constructor(
-    private readonly authenticationService: AuthenticationService,
-    // Should not contains userService business here, this should be encapsulate in authenticationService
-    private readonly usersService: UsersService
+    private readonly authenticationService: AuthenticationService
     ) {}
 
   @Post('/login')
   @UseGuards(LocalGuard)
   login(@Body() loginDto: LoginDto) {
-    
     return this.authenticationService.login(loginDto);
   }
 
-  @Post('/register')
-  register(@Body() createUserDto: CreateUserDto) {
-    /**
-     * Why register create user directly? No validation for registering process?
-     * Use case should be:
-     * - validate username, email, password
-     * - send to user service for user creation
-     */
-    return this.usersService.create(createUserDto);
-  }
 
   @Get('test')
   @UseGuards(JwtAuthGuard, PermissionGuard)
--- src/account-services/authentication/guards/jwt.guard.ts
+++ src/account-services/authentication/guards/jwt.guard.ts
@@ -1,11 +1,31 @@
-import { AuthGuard } from '@nestjs/passport';
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import * as jwt from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
+import { InternalServerErrorException } from 'src/common/exceptions';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        console.log('JwtAuthGuard');
-        return super.canActivate(context);
-    }
+export class JwtAuthGuard implements CanActivate {
+	constructor(private readonly configService: ConfigService) {}
+
+	canActivate(
+		context: ExecutionContext,
+	): boolean | Promise<boolean> | Observable<boolean> {
+		const request = context.switchToHttp().getRequest();
+
+		const token = request.headers.authorization?.split(' ')[1];        
+		if (!token) {
+            return false;
+		}
+
+        try {
+            const secretKey = this.configService.get<string>('JWT_SECRET');
+            const decoded = jwt.verify(token, secretKey);
+            request.user = decoded;
+        } catch (error) {
+            throw new InternalServerErrorException();
+        }
+
+        return true;
+	}
 }
